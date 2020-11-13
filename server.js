4d9ee14ee6a97ec81dce0c3d28cb401497bdde9d
GaikwadRavi03 <55272071+GaikwadRavi03@users.noreply.github.com>
2020-11-13 15:27:48 +0530

    Added: code for jenkins

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const PORT = process.env.PORT || 4000 
+const PORT = process.env.PORT || 4000
 
 require('./db/db.connection')
 
